409b1cdf01a9d6f229ac2d6f368110dfef5114f1
kejaapp <keja.app@outlook.com>
2022-07-20 10:56:07 +0300

    fixing port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const server = http.createServer(app);
 
-const port = process.env.PORT || process.env.API_PORT;
+const port = process.env.PORT || 5000;
 
 server.listen(port,(req,res)=>{
     console.log(`server is listening on http://localhost:${port}`)
