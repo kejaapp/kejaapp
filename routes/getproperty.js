9e04e368bf7826fa45406f89ce756a5326e08859
kejaapp <keja.app@outlook.com>
2022-07-20 11:10:00 +0300

    fixing id

--- routes/getproperty.js
+++ routes/getproperty.js
@@ -5,13 +5,13 @@ let router = express.Router();
 router.post('/', async(req,res)=>{
     //get property id from header/
     const { id } = req.body;
-    //console.log(id);
+    console.log(id);
     //look for existing property 
     if(!id){
         return res.status(401).send('no id found')
     }
     const property = await Property.findOne({_id:id});
-    console.log(property)
+    //console.log(property)
     return res.status(200).json(property)
     //return property
 });
